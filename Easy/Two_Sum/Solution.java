54f24bf91c800335264775cabd2a961e8c4b5997
VinayakBhoir <vinayak.bhoir@mitaoe.ac.in>
2025-10-08 23:34:38 +0530

    two sum file rename

--- Easy/Two_Sum/Solution.java
+++ Easy/Two_Sum/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class TwoSum {
+public class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -14,7 +14,7 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        TwoSum solution = new TwoSum();
+        Solution solution = new Solution();
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
 
