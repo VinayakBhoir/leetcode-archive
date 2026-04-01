d731ce1175ed9bf1393f8c7e215b81bb79bfef9f
VinayakBhoir <vinayak.bhoir@mitaoe.ac.in>
2026-04-01 23:23:36 +0530

    update

--- Easy/Two_Sum/Solution.java
+++ Easy/Two_Sum/Solution.java
@@ -12,7 +12,6 @@ public class Solution {
         }
         return new int[] {};
     }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = { 2, 7, 11, 15 };
