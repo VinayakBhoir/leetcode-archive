c5fdae9fc4fbeb8c349dacbf7d545eded258e371
VinayakBhoir <vinayak.bhoir@mitaoe.ac.in>
2026-06-01 23:35:33 +0530

    problem 2144

---
+++ Easy/Minimum Cost of Buying Candies With Discount/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+
+        int total = 0;
+        int count = 0;
+
+    
+        for(int i=cost.length-1; i>=0;i--){
+            count++;
+            if(count % 3 == 0){
+                continue;
+            }
+            total += cost[i];
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {6, 5, 7, 9, 2, 2};
+
+        Solution sol = new Solution();
+        System.out.println(sol.minimumCost(arr));
+    }
+}
